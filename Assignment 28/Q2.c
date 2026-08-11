87a1767103c47025411d6c93a7b32d2cbe407522
Adesh Aghav <adeshaghav9@gmail.com>
2026-08-11 23:16:36 +0530

    Add files via upload

---
+++ Assignment 28/Q2.c
@@ -0,0 +1,48 @@
+/* Accept Number Rows and Columns from user and Display Below Pattern.
+   Input:   iRow = 4    iCol = 4
+   Output:  *   *   *   #
+            *   *   #   @
+            *   #   @   @
+            #   @   @   @
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0;
+    int j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(j < iCol - i + 1)
+            {
+                printf("*\t");
+            }   
+            else if(i + j == iCol + 1)
+            {
+                printf("#\t");
+            }
+            else
+            {
+                printf("@\t");
+            }
+            
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter Number of Rows and Columns: ");
+    scanf("%d%d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
